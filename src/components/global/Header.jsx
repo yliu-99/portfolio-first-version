b2a48b603fafa4e7159a9bfcb57682a9c4fbbf08
yliu-99 <yuhanliu.0602@gmail.com>
2025-07-27 13:43:29 -0700

    Clean up default files, create folder structure, install SASS

---
+++ src/components/global/Header.jsx
@@ -0,0 +1,27 @@
+function Header() {
+  return (
+    <header>
+      <h1>Welcome to My Website</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/design">Design</a>
+          </li>
+          <li>
+            <a href="/video">Video</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default Header;
