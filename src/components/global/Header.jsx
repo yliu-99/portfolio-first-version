402b87fb1ea6c15d4cf49dd89d5839eeff68956f
yliu-99 <yuhanliu.0602@gmail.com>
2025-07-27 21:51:13 -0700

    set up header nav items, add main grain texture

--- src/components/global/Header.jsx
+++ src/components/global/Header.jsx
@@ -1,26 +1,32 @@
+import "./header.scss";
+
 function Header() {
   return (
     <header>
-      <h1>Welcome to My Website</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/design">Design</a>
-          </li>
-          <li>
-            <a href="/video">Video</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+      <div className="container">
+        <div className="flex space-between">
+            <div className="header-gradient"></div>
+          <nav className="main-menu">
+            <ul>
+              <li>
+                <a href="/about">About</a>
+              </li>
+              <li>
+                <a href="/design">Design</a>
+              </li>
+              <li>
+                <a href="/"><div className="logo"><img src="src\assets\logo\logo-full.png" alt="yuhan liu's logo, y and l combined together to look like an hourglass" /></div>Yuhan Liu</a>
+              </li>
+              <li>
+                <a href="/video">Video</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// Glocal Styles
+import './reset.scss'
+import './main.scss'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
