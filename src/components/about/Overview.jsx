fd03d12cdb80d163c73397c82abb3921404d10c7
yliu-99 <yuhanliu.0602@gmail.com>
2025-07-29 14:51:02 -0700

    updated info on about overview section

--- src/components/about/Overview.jsx
+++ src/components/about/Overview.jsx
@@ -1,8 +1,14 @@
 function Overview() {
   return (
-    <div className="overview">
-      <h2>Overview</h2>
-      <p>This section provides a brief overview of myself</p>
+    <div className="about-overview">
+      <img></img>
+      <h2>About Me</h2>
+      <p>Hi, I’m Yuhan. I am a designer and storyteller and I use creativity as my way of connecting with the world. Currently studying as a New Media Design and Web Development student at BCIT, I am excited to spend each day learning new ways of bringing ideas to life through design, code, and storytelling. Whether I’m building a brand, filming creative content, designing sound environments, or creating striking motion graphics, I always aim to make something that feels meaningful. My biggest inspirations come from film, video games, and music, as well as the storytelling within these mediums that evokes feelings, both vulnerable and beautiful, in people. </p>
+
+    <p>Before shifting into tech and design, I earned a Bachelor’s Degree in Music Performance and also spent two years working as a private voice teacher. These experiences have strengthened my understanding of communication, connection, emotion, and narrative, which I carry into all my creative projects today.
+</p>
+<p>Outside of school and work, I recharge by making music, spending quality time with loved ones, playing video games, cooking, exploring photography, and crafting things by hand. These little joys help me stay curious, grounded, and inspired in everything I do.
+</p>
     </div>
   );
 }
