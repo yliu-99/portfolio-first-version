39462e4db06f1fc01a7b68e055bf30aec5ee897e
yliu-99 <yuhanliu.0602@gmail.com>
2025-07-29 00:47:25 -0700

    update header animation, about section styling, projects styling

--- src/components/home/About.jsx
+++ src/components/home/About.jsx
@@ -7,28 +7,33 @@ function About() {
         <div className="about-content">
           <div className="intro-text">
             <p>I am a</p>
-            <h2 className="multi-disciplinary">MULTI-DISCIPLINARY <br/>DESIGNER</h2>
+            <h2 className="multi-disciplinary">
+              MULTI-DISCIPLINARY <br />
+              DESIGNER
+            </h2>
           </div>
-          
+
           <div className="inspiration-section">
             <p className="inspiration-intro">I take inspiration from</p>
             <div className="inspiration-words">
-             <h4>FILM</h4>
-             <h4>ART</h4>
-             <h4>VIDEO GAMES</h4>
-             <h4>MUSIC</h4>
-
+              <h4>FILM</h4>
+              <h4>ART</h4>
+              <h4>VIDEO GAMES</h4>
+              <h4>MUSIC</h4>
             </div>
-            
+
             <a href="/about" className="more-btn">
               MORE
             </a>
           </div>
         </div>
-        
-        <div className="logo-decoration">
-          <div className="left"><img src="src/assets/logo/logo-full.png" alt="" /></div>
-          <div className="right"><img src="src/assets/logo/logo-full.png" alt="" /></div>
+      </div>
+      <div className="logo-decoration">
+        <div className="left">
+          <img src="src/assets/logo/logo-full.png" alt="" />
+        </div>
+        <div className="right">
+          <img src="src/assets/logo/logo-full.png" alt="" />
         </div>
       </div>
     </div>
