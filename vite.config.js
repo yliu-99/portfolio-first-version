eab994933553ea43bb6a171829e6c2d2314cd48a
yliu-99 <yuhanliu.0602@gmail.com>
2025-07-31 13:36:56 -0700

    publish

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-first-version/', // Replace with your actual repo name
+  build: {
+    outDir: 'dist', // Vite uses 'dist' by default, not 'build'
+  }
 })
