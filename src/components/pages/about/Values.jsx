774a5c2c4f9060b8c5fb644540b4876b250c03db
yliu-99 <yuhanliu.0602@gmail.com>
2025-08-15 19:11:55 -0700

    nav issue and img fix

--- src/components/pages/about/Values.jsx
+++ src/components/pages/about/Values.jsx
@@ -27,7 +27,7 @@ function Values() {
                     <p>My creativity always stems from the sense of wonder I hold for the world. It has always been immensely exciting to me to imagine that the possibilities could be endless, and to explore these opportunities with appreciation. This inspires me to take risks, think further, remain optimistic, and ask more questions in my work.</p>
                   </div>
                   <div className="flip-card-back">
-                    <img src="src/assets/about/yuhan-singing-with-guitar.jpg" alt="Yuhan Singing" />
+                    <img src="https://i.postimg.cc/5y1jBP3q/yuhan-singing-with-guitar.jpg" alt="Yuhan Singing" />
                   </div>
                 </div>
               </div>
@@ -41,7 +41,7 @@ function Values() {
                     <p>To me, love means committing fully to your relationship with people, yourself, and the world at large. I feel most fulfilled when living and leading with love, so I try to carry that principle into everything. In my work, love motivates me to embrace challenges, show gratitude and kindness, act with intention, and make decisions that center in people.</p>
                   </div>
                   <div className="flip-card-back">
-                    <img src="src/assets/about/enqi.jpg" alt="Yuhan's younger brother Enqi" />
+                    <img src="https://i.postimg.cc/fLYJ04xR/enqi.jpg" alt="Yuhan's younger brother Enqi" />
                   </div>
                 </div>
               </div>
@@ -55,7 +55,7 @@ function Values() {
                     <p>Humility, for me, is a way of honoring the vastness of the world around me. It motivates me to be introspective, reflective, and aware in my work. I strive to stay open to learning, growth, and change, and to approach these opportunities with curiosity and excitement, as a part of my commitment to myself in living authentically.</p>
                   </div>
                   <div className="flip-card-back">
-                    <img src="src/assets/about/yuhan-young.JPEG" alt="Child Yuhan" />
+                    <img src="https://i.postimg.cc/7hVhmgtv/yuhan-young.jpg" alt="Child Yuhan" />
                   </div>
                 </div>
               </div>
